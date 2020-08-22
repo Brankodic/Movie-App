1d3b43479efd3751bd6c35afa779e1230271c028
Brankodic <srdjan.brankovicc@gmail.com>
2020-08-23 00:40:57 +0200

    Basic webpack setup added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const template = React.createElement("h1", null, "Movie App");
+
+ReactDom.render(template, document.getElementById("root"));
