a4690b16fa85cc54d99da50f5256fdbb93598ba7
Brankodic <srdjan.brankovicc@gmail.com>
2020-08-25 00:04:19 +0200

    Movie roulette modal setup

--- src/components/MovieRoulette/MovieRoulette.js
+++ src/components/MovieRoulette/MovieRoulette.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import RouletteInput from './RouletteInput/RouletteInput'
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { FaDiceD20 } from "react-icons/fa";
+
+import RouletteInput from "./RouletteInput/RouletteInput";
+
+const useStyles = createUseStyles({
+  h3: {
+    color: "white",
+    textShadow: "1px 2px black",
+    textAlign: "center",
+  },
+  button: {
+    color: "white",
+    marginLeft: 130,
+    marginTop: 30,
+    fontSize: "2.5em",
+    transition: "0.3s",
+    "&:hover": {
+      transition: "0.3s",
+      transform: "scale(1.1)",
+      cursor: "pointer",
+    },
+  },
+});
 
 const MovieRoulette = () => {
-    return (
-        <div>
-            <RouletteInput/>
-            <p>Movie Roullete</p>
-        </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <div className={classes.div}>
+      <h3 className={classes.h3}>Movie Roulette:</h3>
+      <RouletteInput />
+      <FaDiceD20 className={classes.button} />
+    </div>
+  );
 };
 
 export default MovieRoulette;
