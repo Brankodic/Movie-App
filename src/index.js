707cb61b1263db56bbe8262223ce51765d4a34b8
Brankodic <srdjan.brankovicc@gmail.com>
2020-08-29 00:47:12 +0200

    Fixed some auth problems and setup context

--- src/index.js
+++ src/index.js
@@ -2,23 +2,25 @@ import React from "react";
 import ReactDom from "react-dom";
 import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 
+import { AuthProvider } from "./services/AuthContextProvider";
 import Auth from "./services/Auth";
 import Header from "./components/Header/Header";
 import MovieListPage from "./pages/MovieListPage";
 import SingleMoviePage from "./pages/SingleMoviePage";
 
-
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Route exact path="/">
-        <MovieListPage />
-      </Route>
-      <Route path="/single-movie-page">
-        <SingleMoviePage />
-      </Route>
-      <Auth />
+      <AuthProvider >
+        <Header />
+        <Route exact path="/">
+          <MovieListPage />
+        </Route>
+        <Route path="/single-movie-page">
+          <SingleMoviePage />
+        </Route>
+        <Auth />
+      </AuthProvider>
     </Router>
   );
 };
--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,19 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Cookies from "js-cookie";
 
 import AuthButton from "../components/Buttons/AuthButton/AuthButton";
+import { store } from "./AuthContextProvider";
 
 const API_KEY = process.env.API_KEY;
 
 const Auth = () => {
   const [state, setState] = useState({
-    sessionId:Cookies.get("session_id") ,
-    token: Cookies.get("request_token") ,
+    sessionId: Cookies.get("session_id"),
+    token: Cookies.get("request_token"),
     loggedIn: undefined,
   });
 
+  const globalState = useContext(store);
+  const { dispatch } = globalState;
+
   useEffect(() => {
-    if (state.token !== undefined) {
+    if (state.sessionId === undefined) {
+      dispatch({ type: "log out" });
+      setState({ ...state, loggedIn: false });
+    } else {
+      dispatch({ type: "log in" });
+      setState({ ...state, loggedIn: true });
+    }
+  }, [state.sessionId]);
+
+  useEffect(() => {
+    if (state.token !== undefined && state.sessionId === undefined) {
       async function getSessId() {
         const res = await fetch(
           `https://api.themoviedb.org/3/authentication/session/new?api_key=${API_KEY}&request_token=${state.token}`,
@@ -23,23 +37,17 @@ const Auth = () => {
         )
           .then((res) => res.json())
           .then((data) => {
-            Cookies.set("session_id", `${data.session_id}`);
+            Cookies.set("session_id", data.session_id);
             setState({
               ...state,
-              sessionId: Cookies.get("session_id") ,
-              loggedIn: true,
+              sessionId: data.session_id,
             });
           })
           .catch((err) => console.log(err));
       }
       getSessId();
     }
-    if (state.sessionId === undefined) {
-      setState({ ...state, loggedIn: false });
-    } else {
-      setState({ ...state, loggedIn: true });
-    }
-  }, []);
+  }, [state.token]);
 
   const handleLogin = () => {
     async function fetchToken() {
@@ -62,9 +70,8 @@ const Auth = () => {
     Cookies.remove("request_token");
     setState({
       ...state,
-      sessionId:  undefined ,
-      token:  undefined ,
-      loggedIn: false,
+      sessionId: undefined,
+      token: undefined,
     });
   };
 
